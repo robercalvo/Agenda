03d74b7d3ca498baac268e0e7252baaa92b66c4c
Xili <xilisiguenza@gmail.com>
2017-04-04 21:19:03 +0200

    Inicio del proyecto

---
+++ src/es/riberadeltajo/agenda/entidades/Contacto.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.riberadeltajo.agenda.entidades;
+
+import java.util.Objects;
+
+/**
+ * Representa un contacto dentro de la agenda
+ * @author Juanjo Acuña
+ * @version 1.0
+ * @since 1.0
+ */
+public class Contacto implements Comparable
+{
+
+    
+    public enum TIPO{AMIGO,FAMILA,TRABAJO,OTROS};
+    
+    private Long id;
+    private String nombre;
+    private String apellidos;
+    private TIPO tipo;
+    private String telefono;
+    private String email;
+
+    //<editor-fold defaultstate="collapsed" desc="GETTER/SETTER">
+    public Long getId()
+    {
+        return id;
+    }
+    
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+    
+    public String getNombre()
+    {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    public String getApellidos()
+    {
+        return apellidos;
+    }
+    
+    public void setApellidos(String apellidos)
+    {
+        this.apellidos = apellidos;
+    }
+    
+    public TIPO getTipo()
+    {
+        return tipo;
+    }
+    
+    public void setTipo(TIPO tipo)
+    {
+        this.tipo = tipo;
+    }
+    
+    public String getTelefono()
+    {
+        return telefono;
+    }
+    
+    public void setTelefono(String telefono)
+    {
+        this.telefono = telefono;
+    }
+    
+    public String getEmail()
+    {
+        return email;
+    }
+    
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="EQUAL/HASHCODE/COMPARETO">
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Contacto other = (Contacto) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    /**
+     * Compara dos objetos del tipo Contacto
+     * 
+     * @param t Objeto con la que se compara     * 
+     * @return 
+     * <ul>
+     * <li>-1 si es menor que el pasado por parametro</li>
+     * <li>-0 si son iguales</li>
+     * <li>1 si es mayor que el pasado por parametro</li>
+     * </ul>
+     */
+    @Override
+    public int compareTo(Object t)
+    {
+        Contacto c = (Contacto)t;
+        if(c==null || c.getNombre()==null)
+        {
+            throw new IllegalArgumentException();
+        }
+        else
+        {
+            return this.getNombre().compareTo(c.getNombre());
+        }        
+    }
+//</editor-fold>
+    
+    
+    
+
+}
---
+++ src/es/riberadeltajo/agenda/vistas/AgendaTest.java
@@ -0,0 +1,15 @@
+package es.riberadeltajo.agenda.vistas;
+
+/**
+ * Realización de pruebas
+ * @author Juanjo Acuña
+ * @since 1.0
+ * @version 1.0
+ */
+public class AgendaTest 
+{
+    public static void main(String args[])
+    {
+        //TODO Codigo de pruebas
+    }
+}
